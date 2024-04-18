ac45892e648873eb03ae7fc41035fd485560c01f
Алексей <aleksej.erikson@mail.ru>
2024-04-18 20:05:21 +0300

    Изменено

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.10-slim
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+COPY ./templates /code/templates
 COPY ./main.py /code
 WORKDIR /code
 CMD ["uvicorn","main:app","--host","0.0.0.0","--reload", "--port", "80"]
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI(title = "Hello Recruto")
+app = FastAPI(title="Hello Recruto")
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def Hello_Recruto(
-	name: str = Query('Recruto'),
-	message: str = Query('Давай дружить')):
-    return f"Hello {name}! {message}"
+async def hello_recruto(request: Request, name: str = 'Recruto', message: str = 'Давай дружить'):
+    response = templates.TemplateResponse("index.html", {"request": request, "name": name, "message": message})
+    return response
